8f518028c35abe2c9267bf143a955e0cca1fa8de
Giovanni <git@desantolo.com>
2017-05-25 21:15:56 -0700

    cleanup.

--- main_test.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "testing"
-
-// Test reading and interpreting JSON data from file
-// use example: http://danilodellaquila.com/en/blog/read-json-files-in-golang
-func TestReadJSON(t *testing.T) {
-
-}
